95e3c4809af9c9daedf45ef6d806989211fa0f9a
Ghostly-lol <105524594+Ghostly-lol@users.noreply.github.com>
2026-07-26 23:11:56 -0700

    added initial code

---
+++ main.py
@@ -0,0 +1,59 @@
+import time
+import keyboard
+import pynput
+from KeyCodes import *
+
+
+running = False
+
+
+# Countdown before starting
+countdown = 5
+while countdown > 0:
+    print(countdown)
+    countdown -= 1
+    time.sleep(1)
+
+print("Script is running in the background.")
+print("Press NUMPAD 5 to toggle")
+print("Press Shift+Backspace to quit.")
+
+
+
+while True:
+    # Toggle with Numpad 5
+    if keyboard.is_pressed("num 5"):
+        running = not running
+
+        if running:
+            print("Started holding W + E and moveing MOUSE to the right")
+            HoldKey(W)
+            HoldKey(E)
+            # MouseMoveUp(500, .1, 10)
+        else:
+            print("Stopped holding W + E and moveing MOUSE to the right")
+            ReleaseKey(W)
+            ReleaseKey(E)
+            
+
+        # Wait until the key is released so it only toggles once
+        while keyboard.is_pressed("num 5"):
+            time.sleep(0.01)
+
+    # Exit the script
+    if keyboard.is_pressed("shift+backspace"):
+        break
+
+
+    if keyboard.is_pressed("num 0"):
+        print("releasing W + E ")
+        ReleaseKey(W)
+        ReleaseKey(E)
+
+
+    time.sleep(0.01)
+
+# Make sure keys are released before exiting
+ReleaseKey(W)
+ReleaseKey(E)
+print("Exited.")
