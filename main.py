9053539fcdfc64a627df0dcccfb4cf1e57df036c
Ghostly-lol <105524594+Ghostly-lol@users.noreply.github.com>
2026-07-28 19:47:14 -0700

    added version 1 as a callable function

--- main.py
+++ main.py
@@ -59,7 +59,32 @@ def counter():
         count -= 1
         time.sleep(1)
 
+def version_1():
+    while True:
+        # Toggle with Numpad 5
+        if keyboard.is_pressed("num 5"):
+            running = not running
 
+            if running:
+                print("Started holding W + E and moveing MOUSE to the right")
+                HoldKey(W)
+                HoldKey(E)
+                # MouseMove(10,  0.01)
+            else:
+                print("Stopped holding W + E and moveing MOUSE to the right")
+                ReleaseKey(W)
+                ReleaseKey(E)
+                
+
+            # Wait until the key is released so it only toggles once
+            while keyboard.is_pressed("num 5"):
+                time.sleep(0.01)
+
+        # Exit the script
+        if keyboard.is_pressed("shift+backspace"):
+            break
+
+        time.sleep(0.01)
 
 def version_2():
     while True:
